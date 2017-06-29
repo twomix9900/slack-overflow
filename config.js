f891d4268e64621e226912c6fce595c5f36bdfa0
Kay K <ds.kay.kim@gmail.com>
2017-06-29 13:29:32 -0700

    (fix) DB issues

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const dbUrl = 'postgres://klgyjyaf:AJjARPQ60n6bvLFYvDShVy9a1wS9Lz48@stampy.db.elephantsql.com:5432/klgyjyaf';;
+const dbUrl = 'postgres://klgyjyaf:AJjARPQ60n6bvLFYvDShVy9a1wS9Lz48@stampy.db.elephantsql.com:5432/klgyjyaf';
 const secret = '-XahIl2khiGKX03hSIHpJXRCA_htarXmjtEQmg-Paz04cBVA-lQjHEgQBLE7uN6C';
 const audience = 'sI5ktAqaNr9Fo4Mr9NWKPDCQGNE9cGLT';
 
