53bbac761d6afd5d3dd3359b44b799f2b1b6ad0d
Sho Nguyen <sho.nozaki417@gmail.com>
2017-06-30 19:34:22 -0700

    branch for peerjs

--- public/config/auth.config.js
+++ public/config/auth.config.js
@@ -4,8 +4,8 @@
     .module('slackOverflowApp')
     .config(['authProvider', function(authProvider) {
       authProvider.init({
-        domain: 'inseok-ucla.auth0.com',
-        clientID: 'ku4AUn23UfSipuIY4l8e8WovJ10X5XuY'
+        domain: 'nalp.auth0.com',
+        clientID: 'p3YX25mAFylU7F6GadLITKleuETnTKiT'
       });
     }])
 
