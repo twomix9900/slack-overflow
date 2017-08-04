504b9b8ef14a745c266ef2a23c792e5b692aec3c
Kay Kim <ds.kay.kim@gmail.com>
2017-08-03 17:52:13 -0700

    Update auth.config.js

--- auth.config.js
+++ auth.config.js
@@ -1,4 +1,4 @@
-const AUTH0_CLIENT_ID='sI5ktAqaNr9Fo4Mr9NWKPDCQGNE9cGLT'; 
-const AUTH0_DOMAIN='bananafish2828.auth0.com'; 
-const AUTH0_CALLBACK_URL='http://localhost:3000/callback';
+const AUTH0_CLIENT_ID='sMPrpLV62uo43g5t3riJh3a3cprfPWYF'; 
+const AUTH0_DOMAIN='twomix9900.auth0.com'; 
+const AUTH0_CALLBACK_URL='https://stackbetterflow.herokuapp.com/callback';
 
